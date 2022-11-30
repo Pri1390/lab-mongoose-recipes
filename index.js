1d47b61b8ac09948d82d90acd6da5cac5cd8aa15
Priscila Vieira <freedom.pva@gmail.com>
2022-11-30 18:03:12 -0300

    iteração 2 em andamento

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+import express from 'express'
+import * as dotenv from 'dotenv'
+import dbConnect from './config/db.config.js'
+import recipeRoute from './routes/Recipe.routes.js'
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+dotenv.config()
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+dbConnect()
+
+const app = express()
+
+app.use(express.json())
+
+
+app.use('/recipe', recipeRoute)
+app.listen(Number(process.env.PORT), () => console.log('server on port 8080!'))
